72d81083360060bfe9bcb45c3d193d2a8b605c26
Armen <absoboll@gmail.com>
2014-05-27 04:11:41 -0700

    first commit

--- QTTrackCleanup.m
+++ QTTrackCleanup.m
@@ -39,14 +39,27 @@ void deleteTextTracks (Movie theMovie) {
 int main (int argc, const char * argv[]) 
 {
 	// define audio channel label array
-	int channelLabels[] = {kAudioChannelLabel_Left, 
-			kAudioChannelLabel_Right,
-			kAudioChannelLabel_Center,
-			kAudioChannelLabel_LFEScreen,
-			kAudioChannelLabel_LeftSurround,
-			kAudioChannelLabel_RightSurround,
-			kAudioChannelLabel_LeftTotal,
-		kAudioChannelLabel_RightTotal };
+	int fourChanLabels[] = 
+	{
+		kAudioChannelLabel_LeftTotal,
+		kAudioChannelLabel_RightTotal,
+		kAudioChannelLabel_LeftTotal,
+		kAudioChannelLabel_RightTotal
+	};
+	
+	int channelLabels[] = 
+	{
+		kAudioChannelLabel_Left, 
+		kAudioChannelLabel_Right,
+		kAudioChannelLabel_Center,
+		kAudioChannelLabel_LFEScreen,
+		kAudioChannelLabel_LeftSurround,
+		kAudioChannelLabel_RightSurround,
+		kAudioChannelLabel_LeftTotal,
+		kAudioChannelLabel_RightTotal,
+		kAudioChannelLabel_LeftTotal,
+		kAudioChannelLabel_RightTotal
+	};
 	
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 	NSArray *args = [[NSProcessInfo processInfo] arguments];
@@ -67,58 +80,119 @@ int main (int argc, const char * argv[])
 		NSLog(@"**Error** Problems opening file");
 		return 1;
 	}
-	
-	// Some problem with QTKit and -tracks on a QTMovie. Bus Error.
-	// So drop down into the native QuickTime API	
 	Movie qtMovie = [movie quickTimeMovie];	
 	
-	// loop through audio tracks
-	for (int currentTrack = 1, audioTrackCounter = 0; currentTrack <= 8; currentTrack++) {
-		
+	int audioTrackCounter = 0;
+	for (int currentTrack = 1; currentTrack <= 10; currentTrack++) {
 		Track audioTrack = GetMovieIndTrackType(qtMovie, currentTrack, SoundMediaType, movieTrackMediaType);
-		if (audioTrack == NULL) {
-			// remove any text tracks, if they exist
-			deleteTextTracks(qtMovie);
-			// Save the movie to disk
-			[movie updateMovieFile];
-			
-			return 0;
-		}		
+		if (audioTrack == NULL) 
+		{
+			// get number of audio tracks
+			break;
+		}
 		
-		AudioChannelLayout* trackChannelLayout = NULL;
-		OSStatus err = noErr;
-		UInt32 trackChannelLayoutSize;
-		
-		// Allocate a layout of the required size
-		trackChannelLayoutSize = fieldOffset(AudioChannelLayout, mChannelDescriptions[1]);
-		trackChannelLayout = (AudioChannelLayout*)calloc(1, trackChannelLayoutSize);
-		trackChannelLayout->mChannelLayoutTag = kAudioChannelLayoutTag_UseChannelDescriptions;
-		
-		// 1 audio channel per track
-		trackChannelLayout->mNumberChannelDescriptions = 1;				
-		trackChannelLayout->mChannelDescriptions[0].mChannelLabel = channelLabels[audioTrackCounter];
 		audioTrackCounter++;
-		
-		// Set the track layout
-		err = QTSetTrackProperty(audioTrack, 
-								 kQTPropertyClass_Audio,
-								 kQTAudioPropertyID_ChannelLayout, 
-								 trackChannelLayoutSize, 
-								 trackChannelLayout);
-		if (err != noErr) {
-			NSLog(@"**Error** QuickTime SetPropertyError: %i", err);
-			return 1;
-		}		
-
-		//printf("labeled audio track %d as %s\n", currentTrack, channelLabels[audioTrackCounter]);
-
 	}
 	
+	NSLog(@"Tracks: %i", audioTrackCounter);
+	// Some problem with QTKit and -tracks on a QTMovie. Bus Error.
+	// So drop down into the native QuickTime API	
+	if (audioTrackCounter == 4)
+	{
+		for (int currentTrack = 1, audioTrackCounter = 0; currentTrack <= 4; currentTrack++) 
+		{
+			
+			Track audioTrack = GetMovieIndTrackType(qtMovie, currentTrack, SoundMediaType, movieTrackMediaType);
+			if (audioTrack == NULL) 
+			{
+				// remove any text tracks, if they exist
+				deleteTextTracks(qtMovie);
+				// Save the movie to disk
+				[movie updateMovieFile];
+				
+				return 0;
+			}		
+			
+			AudioChannelLayout* trackChannelLayout = NULL;
+			OSStatus err = noErr;	
+			UInt32 trackChannelLayoutSize;
+			
+			// Allocate a layout of the required size
+			trackChannelLayoutSize = fieldOffset(AudioChannelLayout, mChannelDescriptions[1]);
+			trackChannelLayout = (AudioChannelLayout*)calloc(1, trackChannelLayoutSize);
+			trackChannelLayout->mChannelLayoutTag = kAudioChannelLayoutTag_UseChannelDescriptions;
+			
+			// 1 audio channel per track
+			trackChannelLayout->mNumberChannelDescriptions = 1;				
+			trackChannelLayout->mChannelDescriptions[0].mChannelLabel = fourChanLabels[audioTrackCounter];
+			audioTrackCounter++;
+			
+			// Set the track layout
+			err = QTSetTrackProperty(audioTrack, 
+									 kQTPropertyClass_Audio,
+									 kQTAudioPropertyID_ChannelLayout, 
+									 trackChannelLayoutSize, 
+									 trackChannelLayout);
+			if (err != noErr) 
+			{
+				NSLog(@"**Error** QuickTime SetPropertyError: %i", err);
+				return 1;
+			}		
+			
+			//printf("labeled audio track %d as %s\n", currentTrack, channelLabels[audioTrackCounter]);
+		}
+	} else {
+			
+	// loop through audio tracks
+		for (int currentTrack = 1, audioTrackCounter = 0; currentTrack <= 10; currentTrack++) 
+		{
+			
+			Track audioTrack = GetMovieIndTrackType(qtMovie, currentTrack, SoundMediaType, movieTrackMediaType);
+			if (audioTrack == NULL) 
+			{
+				// remove any text tracks, if they exist
+				deleteTextTracks(qtMovie);
+				// Save the movie to disk
+				[movie updateMovieFile];
+				
+				return 0;
+			}		
+			
+			AudioChannelLayout* trackChannelLayout = NULL;
+			OSStatus err = noErr;	
+			UInt32 trackChannelLayoutSize;
+			
+			// Allocate a layout of the required size
+			trackChannelLayoutSize = fieldOffset(AudioChannelLayout, mChannelDescriptions[1]);
+			trackChannelLayout = (AudioChannelLayout*)calloc(1, trackChannelLayoutSize);
+			trackChannelLayout->mChannelLayoutTag = kAudioChannelLayoutTag_UseChannelDescriptions;
+			
+			// 1 audio channel per track
+			trackChannelLayout->mNumberChannelDescriptions = 1;				
+			trackChannelLayout->mChannelDescriptions[0].mChannelLabel = channelLabels[audioTrackCounter];
+			audioTrackCounter++;
+			
+			// Set the track layout
+			err = QTSetTrackProperty(audioTrack, 
+									 kQTPropertyClass_Audio,
+									 kQTAudioPropertyID_ChannelLayout, 
+									 trackChannelLayoutSize, 
+									 trackChannelLayout);
+			if (err != noErr) 
+			{
+				NSLog(@"**Error** QuickTime SetPropertyError: %i", err);
+				return 1;
+			}		
+
+			//printf("labeled audio track %d as %s\n", currentTrack, channelLabels[audioTrackCounter]);
+
+		}
+	}
 	
 	// make sure there were ONLY 8 audio tracks
-	Track audioTrack = GetMovieIndTrackType(qtMovie, 9, SoundMediaType, movieTrackMediaType);
+	Track audioTrack = GetMovieIndTrackType(qtMovie, 11, SoundMediaType, movieTrackMediaType);
 	if (audioTrack != NULL) {
-		NSLog(@"**Error** There are more than 8 audio tracks in the input file.");
+		NSLog(@"**Error** There are more than 10 audio tracks in the input file.");
 		return 1;
 	}		
 	
